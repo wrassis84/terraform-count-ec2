70ccc7433f4e0d725d5c9402c76b7a73d24c6577
William Rezende <wrassis84@gmail.com>
2022-09-28 20:09:04 -0300

    docs: second commit

--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "node" {
   # Creates four identical aws ec2 instances
-    count = 4
+  count = 4
 
   # All four instances will have the same ami and instance_type
-    ami = lookup(var.ec2_ami,var.region)
-    instance_type = var.instance_type #
-	    
-    tags = {
-      # The count.index allows you to launch a resource
-      # Starting with the distinct index number 0 and
-      # corresponding to this instance.
-      Name = "node-$(count.index)"
-    }
+  ami           = lookup(var.ec2_ami, var.region)
+  instance_type = var.instance_type #
+
+  tags = {
+    # The count.index allows you to launch a resource
+    # Starting with the distinct index number 0 and
+    # corresponding to this instance.
+    Name = "node-$(count.index)"
+  }
 }
---
+++ vars.tf
@@ -0,0 +1,20 @@
+# Creating a Variable for ami of type map:
+
+variable "ec2_ami" {
+  type = map(any)
+
+  default = {
+    us-east-2 = "ami-0416962131234133f"
+    us-west-1 = "ami-006fce872b320923e"
+  }
+}
+
+# Creating a Variable for region:
+variable "region" {
+  default = us-east-2
+}
+
+# Creating a Variable for instance_type:
+variable "instance_type" {
+  type = string
+}
