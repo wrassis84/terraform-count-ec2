ba8527182b926ea54129005e33221ebbb1041aad
William Rezende <wrassis84@gmail.com>
2022-09-28 21:23:44 -0300

    docs: changes default value for region variable and .gitignore file

--- vars.tf
+++ vars.tf
@@ -5,13 +5,14 @@ variable "ec2_ami" {
 
   default = {
     us-east-2 = "ami-0416962131234133f"
-    us-west-1 = "ami-006fce872b320923e"
+    us-west-1 = "ami-07ebfd5b3428b6f4d"
   }
 }
 
 # Creating a Variable for region:
 variable "region" {
-  default = us-east-1
+  description = "Define what region the instance will be deployed"
+  default = "us-east-1"
 }
 
 # Creating a Variable for instance_type:
