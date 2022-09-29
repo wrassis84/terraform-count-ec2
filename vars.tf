c6e144d84d21e5d15180ec08c2d4da56a541e1ac
William Rezende <wrassis84@gmail.com>
2022-09-29 20:24:58 -0300

    docs: just fmt .tf files

--- vars.tf
+++ vars.tf
@@ -1,22 +1,22 @@
 # Creating a Variable for ami:
 variable "ec2_ami" {
   description = "AWS AMI to be used "
-  default = "ami-07ebfd5b3428b6f4d"
+  default     = "ami-07ebfd5b3428b6f4d"
 }
 
 # Creating a Variable for region:
 variable "region" {
   description = "Define what region the instance will be deployed"
-  default = "us-east-1"
+  default     = "us-east-1"
 }
 
 # Creating a Variable for instance_type:
 variable "instance_type" {
   description = "Defines the hardware configuration of the machine"
-  type = string
+  type        = string
 }
 
 variable "env" {
   description = "Environment of the Application"
-  default = "staging"
+  default     = "staging"
 }
